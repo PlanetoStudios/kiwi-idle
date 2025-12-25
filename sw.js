04afb8a3d06937b6e48664ddd290e1516fa24e17
PlanetoStudios <carvalhomarandubay@gmail.com>
2025-12-25 14:39:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
-const CACHE_NAME = "kiwi-idle-v1";
-const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
+const CACHE = "kiwi-idle-v2";
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll(["./","./index.html","./manifest.json"])
+    )
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
